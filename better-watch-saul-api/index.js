7e65589995a1421d8bbb692abf5beff16c1b1bde
Eric Navar <ericnavar@ufl.edu>
2023-05-05 16:59:34 -0400

    connect to database

--- better-watch-saul-api/index.js
+++ better-watch-saul-api/index.js
@@ -2,10 +2,19 @@ const serverless = require("serverless-http");
 const express = require("express");
 const tracks = require('./routes');
 const cors = require('cors');
-// const connectDB = require('./connect');
+const connectDB = require('./connect');
 require('dotenv').config();
 
+const connect = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI);
+    } catch (error) {
+      console.log(error);
+    }
+};
+
 const app = express();
+connect();
 
 app.use(cors());
 app.use('/api/v1/tracks', tracks);
