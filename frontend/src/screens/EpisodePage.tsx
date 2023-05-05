802328ed05258d1755414520cf72c40785880544
Eric Navar <ericnavar@ufl.edu>
2023-05-05 16:12:35 -0400

    Add thumbnail to video player

--- frontend/src/screens/EpisodePage.tsx
+++ frontend/src/screens/EpisodePage.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import BetterWatchSaulLogo from '../assets/better-watch-saul-logo.png';
-import { Typography } from '@mui/material';
+import { Typography, styled } from '@mui/material';
 import { EpisodesGroup } from '../components/EpisodesGroup';
 import { Episode } from '../commonTypes';
 import { getEpisode } from '../util';
 
-const EpisodePage = (props: {trackId: string}) => {
-    const [episode, setEpisode] = React.useState<Episode|null>(null);
+const EpisodePage = (props: { trackId: string }) => {
+    const [episode, setEpisode] = React.useState<Episode | null>(null);
 
     React.useEffect(() => {
-      const fetchEpisodes = async () => {
-        let response;
-        response = await getEpisode('4');
-        console.log(response.data);
-        if (response && response.data && response.data)
-            setEpisode(response.data);
-      };
-      fetchEpisodes();
+        const fetchEpisodes = async () => {
+            let response;
+            response = await getEpisode('4');
+            console.log(response.data);
+            if (response && response.data && response.data)
+                setEpisode(response.data);
+        };
+        fetchEpisodes();
     }, [props]);
 
     if (episode === null) {
@@ -24,24 +24,29 @@ const EpisodePage = (props: {trackId: string}) => {
     }
 
     return (
-        <div style={{padding: 20}}>
+        <div style={{ padding: 20 }}>
             <a href='/'>
-                <img src={BetterWatchSaulLogo} alt="Better Watch Saul" width="250px"/>
+                <img src={BetterWatchSaulLogo} alt="Better Watch Saul" width="250px" />
             </a>
             <br />
-            <video width="700px" height="400px" controls>
+            <video
+                width="700px"
+                height="400px"
+                controls
+                poster={`https://d34lypc6o619vf.cloudfront.net/${episode.season_number}.${episode.episode_number}.jpg`}
+            >
                 <source src={episode.src} type="video/mp4" />
             </video>
-            <Typography variant='subtitle1' style={{fontSize: 18}}>
-                Season {episode.season_number} Episode {episode.episode_number} 
+            <Typography variant='subtitle1' style={{ fontSize: 18 }}>
+                Season {episode.season_number} Episode {episode.episode_number}
             </Typography>
             <Typography variant='body1'>
                 {episode.description}
             </Typography>
-            <br/>
-            <EpisodesGroup defaultSeason={episode.season_number}/>
+            <br />
+            <EpisodesGroup defaultSeason={episode.season_number} />
         </div>
     );
 }
 
-export {EpisodePage};
+export { EpisodePage };
